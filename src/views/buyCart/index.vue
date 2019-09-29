a79856ed9e02dad783b252d46dfbc859ac075f10
Caihaofei <350019619@qq.com>
2019-09-29 17:30:50 +0800

    Initial commit

---
+++ src/views/buyCart/index.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="cart_wrap">
+    <main>
+        <CartCode ></CartCode>
+    </main>
+    <footer></footer>
+      
+  </div>
+</template>
+
+<script>
+import CartCode from '../../components/cartListCode/'
+export default {
+  components:{
+    CartCode
+  },
+  computed:{
+    
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.cart_wrap{
+  width: 100%;
+  height: 100%;
+  background: #f0f0ff;
+  display: flex;
+  flex-direction: column;
+}
+main{
+  flex: 1;
+}
+footer{
+  width: 100%;
+  height: 60px;
+  border-top: 1px solid #666;
+}
+</style>
